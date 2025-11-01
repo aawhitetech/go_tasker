53a77950dff1b90e4867a1f391bc503061a00863
Aaron White <aawhitetech@gmail.com>
2025-11-01 16:01:48 -0400

    store tasks in struct array

--- tasker/main.go
+++ tasker/main.go
@@ -7,10 +7,18 @@ import (
 
 func PrintUsage() {
 	fmt.Println("Usage: tasker <command> [arguments]")
-    fmt.Println("Commands:")
-    fmt.Println("  add <task description>")
-    fmt.Println("  list")
+	fmt.Println("Commands:")
+	fmt.Println("  add <task description>")
+	fmt.Println("  list")
 }
+
+type Task struct {
+	ID          int
+	Description string
+	Done        bool
+}
+
+var tasks []Task
 
 func main() {
 	fmt.Println("tasker running")
@@ -23,24 +31,36 @@ func main() {
 		return
 	}
 
-	cmd:= args[0]
+	cmd := args[0]
 
 	switch cmd {
-		case "add":
-			if len(args) < 2 {
-				fmt.Println("Error: No Task Name Provided")
-				fmt.Println("Usage: tasker add <task description>")
-				return
-			}
-			taskDescription := args[1]
-			fmt.Printf("Added task: %s\n", taskDescription)
-
-		case "list":
-			fmt.Println("Listing tasks: (none yet)")
-
-		default:
-			fmt.Printf("Error: Unknown Command: %q\n", args[0])
-			PrintUsage()
+	case "add":
+		if len(args) < 2 {
+			fmt.Println("Error: No Task Name Provided")
+			fmt.Println("Usage: tasker add <task description>")
 			return
+		}
+		taskDescription := args[1]
+		newTask := Task {
+			ID: len(tasks) + 1,
+			Description: taskDescription,
+			Done: false,
+		}
+		tasks = append(tasks, newTask)
+		fmt.Printf("Added task: %d. [%v] %s\n", newTask.ID, newTask.Done, newTask.Description)
+
+	case "list":
+		if len(tasks) == 0 {
+			fmt.Println("Listing tasks: (none yet)")
+			return
+		}
+		for _, v := range tasks {
+			fmt.Printf("%d. [%v] %s\n", v.ID, v.Done, v.Description)
+		}
+
+	default:
+		fmt.Printf("Error: Unknown Command: %q\n", args[0])
+		PrintUsage()
+		return
 	}
 }
